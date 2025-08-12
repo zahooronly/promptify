eb814c2cd0be994363aab7dfaa357fc6f244d053
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-12 10:51:16 +0500

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-    },
-    watch: {
-      usePolling: true,
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        'service-worker': resolve(__dirname, 'src/service-worker.ts'),
-        content: resolve(__dirname, 'src/content/index.tsx'),
-      },
-      output: {
-        entryFileNames: '[name].js',
-      },
-    },
-  },
+  plugins: [react()],
 })
